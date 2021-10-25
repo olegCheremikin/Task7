54680afd4ed802354dec40aad17e7b5375b55d6b
O.Cheremikin <O.Cheremikin@CZC724BBKX>
2021-10-25 16:35:47 +0300

    Добавьте файлы проекта.

---
+++ Task7.2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task7._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            double V, S;
+            GetParam(a, out V, out S);
+            Console.WriteLine("Объем куба = {0}", V);
+            Console.WriteLine("Площадь поверхности куба = {0}", S);
+            Console.ReadKey();
+        }
+        static void GetParam(int a, out double V, out double S)
+        {
+            V = a * a * a;
+            S = 6 * a * a;
+        }
+    }
+}
---
+++ Task7/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a1 = double.Parse(Console.ReadLine());
+            double b1 = double.Parse(Console.ReadLine());
+            double c1 = double.Parse(Console.ReadLine());
+            double a2 = double.Parse(Console.ReadLine());
+            double b2 = double.Parse(Console.ReadLine());
+            double c2 = double.Parse(Console.ReadLine());
+            if ((S(a1, b1, c1) > S(a2, b2, c2)))
+            {
+                Console.WriteLine("Площадь первого треуголька больше");
+            }
+            else
+            {
+                Console.WriteLine("Площадь второго треуголька больше");
+            }
+            Console.Read();
+        }
+        static double S(double a, double b, double c)
+        {
+            double p = (a + b + c) / 2;
+            double S = Math.Sqrt(Math.Abs(p * (p - a) * (p - b) * (p - c)));
+            return S;
+        }
+    }
+}
